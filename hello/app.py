84131819b10180f964c0ff4446033471ca53498d
Kevin <neoglitch822@yahoo.com>
2025-02-25 15:34:05 -0500

    Update app.py

--- hello/app.py
+++ hello/app.py
@@ -1,14 +1,11 @@
-# "flask run" to run and right click link in terminal. Don't deploy on test server more than a few times per minute. Otherwise it won't load
+# "flask run" to run and ctrl-click link in terminal.
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        name = request.form.get("name")
+        return render_template("greet.html", name=name)
     return render_template("index.html", placeholder="name")
-
-
-@app.route("/greet", methods=["POST"])
-def greet():
-    name = request.form.get("name", "world")
-    return render_template("greet.html", name=name)
