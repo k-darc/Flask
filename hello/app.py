60bf5f061edcb59354246fe1c412e6c151cc09dd
Kevin <neoglitch822@yahoo.com>
2025-02-25 15:09:23 -0500

    Update app.py

--- hello/app.py
+++ hello/app.py
@@ -1,8 +1,14 @@
+# "flask run" to run and right click link in terminal. Don't deploy on test server more than a few times per minute. Otherwise it won't load
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return render_template("index.html", placeholder="name")
+
+
+@app.route("/greet")
+def greet():
     name = request.args.get("name", "world")
-    return render_template("index.html", placeholder=name)
+    return render_template("greet.html", name=name)
