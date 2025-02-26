f4985e60134f3745b2776d1f81c2dcda692917d8
Kevin <neoglitch822@yahoo.com>
2025-02-26 14:46:00 -0500

    Update app.py

--- froshims/app.py
+++ froshims/app.py
@@ -3,12 +3,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+SPORTS = ["Basketball", "Soccer", "Ultimate Frisbee"]
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports=SPORTS)
+
 
 @app.route("/register", methods=["POST"])
 def register():
-    if not request.form.get("name") or not request.form.get("sport"):
+    if not request.form.get("name"):
         return render_template("failure.html")
+    for sport in request.form.getlist("sport"):
+        if sport not in SPORTS:
+            return render_template("failure.html")
     return render_template("success.html")
