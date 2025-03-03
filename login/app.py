63083e548ea36149c23753569461abd4e5cd297c
Kevin <neoglitch822@yahoo.com>
2025-03-03 14:01:02 -0500

    Update app.py

--- login/app.py
+++ login/app.py
@@ -12,9 +12,14 @@ Session(app)
 def index():
     return render_template("index.html", name=session.get("name"))
 
-app.route("/login")
+app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         session["name"] = request.form.get("name")
         return redirect("/")
     return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
