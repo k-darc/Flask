f814353d0882bee4d1191b3848d1263099024b80
Kevin <neoglitch822@yahoo.com>
2025-03-03 13:44:08 -0500

    Update app.py

--- login/app.py
+++ login/app.py
@@ -7,6 +7,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+REGISTRANTS = {}
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -14,5 +16,6 @@ def index():
 app.route("/login")
 def login():
     if request.method == "POST":
-        session
+        session["name"] = request.form.get("name")
+        return redirect("/")
     return render_template("login.html")
